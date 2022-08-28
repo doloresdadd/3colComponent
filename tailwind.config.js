896d8d8ac043302e6818f794a912533fe6997fbb
Bethanie Innes <doloresdadd@gmail.com>
2022-08-28 18:12:27 +1000

    Frontend mentor content

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brightOrange: "hsl(31, 77%, 52%)",
+        darkCyan: "hsl(184, 100%, 22%)",
+        veryDarkCyan: "hsl(179, 100%, 13%)",
+        transparentWhite: "hsla(0, 0%, 100%, 0.75)",
+        veryLightGray: "hsl(0, 0%, 95%)",
+      },
+      fontFamily: {
+        sans: ["Lexend Deca", "sans-serif"],
+        bigShoulders: ["Big Shoulders Display", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
